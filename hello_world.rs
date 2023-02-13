4d8df089dec0f183b8c08d22eb3aee516de23636
SOK14 <yoshihiro_tsujii@yahoo.co.jp>
2023-02-13 22:53:19 +0900

    update

--- hello_world.rs
+++ hello_world.rs
@@ -2,6 +2,28 @@
 //   print!("hello world!")
 // }
 
+// fn main() {
+//   println!("Welcome to the playground! You can modify the code in here.");
+// }
+
+// fn main() {
+//   // rust infers the type of x
+//   let x = 13;
+//   println!("{}", x);
+
+//   // rust can also be explicit about the type
+//   let x: f64 = 3.14159;
+//   println!("{}", x);
+
+//   // rust can also declare and initialize later, but this is rarely done
+//   let x;
+//   x = 0;
+//   println!("{}", x);
+// }
+
 fn main() {
-  println!("Welcome to the playground! You can modify the code in here.");
+  let mut x = 42;
+  println!("{}", x);
+  x = 13;
+  println!("{}", x);
 }
